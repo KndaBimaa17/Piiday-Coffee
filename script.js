ca3109c2491bd2780a7a129eb13ff7e18acb24d2
KndaBimaa17 <muhammadkanda100505@gmail.com>
2023-12-17 20:43:05 +0700

    commit 17 dec 23

--- script.js
+++ script.js
@@ -10,3 +10,55 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+// SCROLL BUTTON
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 1000 ||
+    document.documentElement.scrollTop > 1000
+  ) {
+    document.getElementById("scrollButton").style.display = "block";
+  } else {
+    document.getElementById("scrollButton").style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+// SCROLL BUTTON END
+
+// POP UP START
+const authorSpan = document.getElementById("author");
+const popup = document.getElementById("popup");
+const closeBtn = document.getElementById("closeBtn");
+
+authorSpan.addEventListener("click", function () {
+  popup.style.display = "block";
+});
+
+closeBtn.addEventListener("click", function () {
+  popup.style.display = "none";
+});
+// POP UP END
+
+// SUMMARY POPUP
+const summaryidSpan = document.getElementById("summaryid");
+const popup2 = document.getElementById("popup2");
+const closeBtnPopup2 = document.getElementById("closeBtn2");
+
+summaryidSpan.addEventListener("click", function () {
+  popup2.style.display = "block";
+});
+
+closeBtnPopup2.addEventListener("click", function () {
+  popup2.style.display = "none";
+});
+// SUMMARY POPUP END
